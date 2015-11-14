8cc3aa7125e3334508d8731e7947c3b37e6547d5
paruff <phil.ruff@gmail.com>
2015-11-14 16:41:35 -0500

    init

---
+++ infrastructure/chef/.chef/knife.rb
@@ -0,0 +1,13 @@
+# See http://docs.chef.io/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "#{user}"
+client_key               "#{current_dir}/#{user}.pem"
+validation_client_name   "validator"
+validation_key           "#{current_dir}/validator.pem"
+chef_server_url          "https://api.chef.io/organizations/"
+cookbook_path            ["#{current_dir}/../cookbooks"]
+knife[:winrm_port] = 5985
+knife[:identity_file] = "sandbox" 
