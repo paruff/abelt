54bf638d43c233366d63320e8ce5a17e9c70a8db
Phil Ruff <phil.ruff@scaledmarkets.com>
2018-02-02 11:53:17 -0500

    changing name to elk adding ports

--- infrastructure/vagrant/virtualbox/elk/spec/elk_spec.rb
+++ infrastructure/vagrant/virtualbox/elk/spec/elk_spec.rb
@@ -1,6 +1,14 @@
 require 'spec_helper'
 
 # Verify packages
-describe package('chef') do
+describe package('elastic') do
     it { should be_installed.with_version('1.8.3') }
   end
+
+describe host('elk') do
+  it { should be_reachable.with( :port => 9200 ) }
+end
+
+describe port(9200) do
+  it { should be_listening }
+end
