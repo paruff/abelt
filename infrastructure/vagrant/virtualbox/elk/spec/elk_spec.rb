c56dca2502047cef0699fb8fd369a199cb6e2d9b
Phil Ruff <phil.ruff@scaledmarkets.com>
2018-02-02 14:43:38 -0500

    updating ELK java, ntp, kibana and logstash and specs

--- infrastructure/vagrant/virtualbox/elk/spec/elk_spec.rb
+++ infrastructure/vagrant/virtualbox/elk/spec/elk_spec.rb
@@ -1,14 +1,53 @@
 require 'spec_helper'
 
 # Verify packages
-describe package('elastic') do
-    it { should be_installed.with_version('1.8.3') }
-  end
 
-describe host('elk') do
-  it { should be_reachable.with( :port => 9200 ) }
+describe package('ntp') do
+  it { should be_installed }
 end
 
-describe port(9200) do
-  it { should be_listening }
+describe "java" do
+  describe command('java -version') do
+    its(:exit_status) { should eq 0 }
+  end
+end
+  describe package('elasticsearch') do
+    it "is enabled" do
+      expect(service("elasticsearch")).to be_enabled
+    end
+  
+    it "is running" do
+      expect(service("elasticsearch")).to be_running.under('init')
+    end  
+  
+    it "is listening on port 9200" do
+      expect(port(9200)).to be_listening
+    end  
+    
+    describe port(9300) do
+      it { should be_listening }
+    end
+
+  end
+
+describe "logstash service" do
+  it "is enabled" do
+    expect(service("logstash")).to be_enabled
+  end
+
+  it "is running" do
+    expect(service("logstash")).to be_running.under('init')
+  end  
+
+end
+
+# kibana
+describe "kibana service" do
+  it "is running" do
+    expect(service("kibana")).to be_running.under('runit')
+  end
+
+  it "is listening on port 5602" do
+    expect(port(5602)).to be_listening
+  end
 end
